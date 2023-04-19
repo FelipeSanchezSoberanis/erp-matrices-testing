2a2db5de174da90a59ac35cd3a4460bfaa7f42eb
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-04-19 11:09:45 -0600

    Initial commit

---
+++ src/main/java/com/proyect/erpMatricesJson/ErpMatricesJsonApplication.java
@@ -0,0 +1,12 @@
+package com.proyect.erpMatricesJson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpMatricesJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpMatricesJsonApplication.class, args);
+    }
+}
---
+++ src/test/java/com/proyect/erpMatricesJson/ErpMatricesJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.erpMatricesJson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpMatricesJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
